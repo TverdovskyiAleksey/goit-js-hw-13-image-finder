6adf0c4109202283a2c577fe31a5e0d35db74ab4
Alex <tverdovskyi92@gmail.com>
2021-05-27 23:26:00 +0300

    changes

---
+++ src/apiService.js
@@ -0,0 +1,29 @@
+const PAGE_KEY = '21816911-1420e6eb818d750af174e21f8';
+export default class CardApiService {
+    constructor() {
+        this.searchQuery = '';
+        this.page = 1;
+    }
+    
+    fetchArticles() {
+    return fetch(`https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=12&key=${PAGE_KEY}`)
+        .then(r => r.json())
+        .then(data => {
+            this.page += 1;
+            return data.hits;
+        });
+    }
+
+    resetPage() {
+        this.page = 1;
+    }
+
+    get query() {
+        return this.searchQuery;
+    }
+
+    set query(newQuery) {
+        this.searchQuery = newQuery;
+    }
+}
+
--- src/index.js
+++ src/index.js
@@ -1,1 +1,47 @@
 import './sass/main.scss';
+import hitsTpl from './templates/hits.hbs';
+import CardApiService from './apiService';
+import { debounce } from 'lodash';
+import * as basicLightbox from 'basiclightbox';
+
+const searchForm = document.querySelector('.search-form');
+const loadMoreBtn = document.querySelector('.btn');
+const articlesContainer = document.querySelector('.gallery');
+const cardApiService = new CardApiService();
+
+searchForm.addEventListener('input', debounce(onSearch, 500));
+loadMoreBtn.addEventListener('click', onLoadMore);
+articlesContainer.addEventListener('click', onClick);
+
+function onSearch(e) {
+    e.preventDefault();
+
+    clearHitsContainer()
+    cardApiService.query = e.target.value;
+    cardApiService.resetPage();
+    cardApiService.fetchArticles().then(appendArticlesMarkup);
+}
+
+function onLoadMore() {
+    cardApiService.fetchArticles().then(appendArticlesMarkup);
+}
+
+function appendArticlesMarkup(hits) {
+    articlesContainer.insertAdjacentHTML('beforeend', hitsTpl(hits));
+    articlesContainer.scrollIntoView({
+  behavior: 'smooth',
+  block: 'end',
+});
+}
+
+function clearHitsContainer() {
+    articlesContainer.innerHTML = '';
+}
+
+function onClick(e) {
+    if (e.target.nodeName !== "IMG") {
+        return;
+    }
+    const ref = e.target.dataset.ref;
+    const instance = basicLightbox.create(`<img src='${ref}' alt='${1}'>`).show();
+}
